1a3dc04fc93fe80fcd728b32986676d59dd5c92e
Débora <d.saioron@hotmail.com>
2022-08-11 21:31:48 -0300

    criando funções de jogar e mudar o simbolo

--- jogo-da-velha.js
+++ jogo-da-velha.js
@@ -1,12 +1,28 @@
 const todasCelulas = document.querySelectorAll("[data-celula]");
+const container = document.querySelector("[data-container]")
 
-for (const celula of todasCelulas) {
-    celula.addEventListener("click", jogando, { once: true});
+let marcadoCirculo = false;
+
+function marcandoEspaco(celula, adicionandoClasse) {
+    celula.classList.add(adicionandoClasse);
 }
 
-function jogando() {
+function mudandoSimbolo() {
+    marcadoCirculo = !marcadoCirculo
+}
+
+function jogando(e) {
     //colocar o X ou Bolinha
+    const celula = e.target;
+    const adicionandoClasse = marcadoCirculo ? "circulo" : "x";
+ 
+    marcandoEspaco (celula, adicionandoClasse);
     //verificar por vitória
     //verificar por empate
     //mudar símbolo
+    mudandoSimbolo();
+}
+
+for (const celula of todasCelulas) {
+    celula.addEventListener("click", jogando, { once: true});
 }
