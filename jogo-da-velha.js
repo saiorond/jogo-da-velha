04fb37a9e4e1ea22c22e0e065e45e43bb2ccd0c7
Débora <d.saioron@hotmail.com>
2022-08-11 21:51:03 -0300

    criando funções de iniciar e o array

--- jogo-da-velha.js
+++ jogo-da-velha.js
@@ -1,15 +1,50 @@
 const todasCelulas = document.querySelectorAll("[data-celula]");
 const container = document.querySelector("[data-container]")
 
-let marcadoCirculo = false;
+let marcadoCirculo;
+
+const combinacoesVitoriosas = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+function iniciarJogo() {
+    for (const celula of todasCelulas) {
+        celula.addEventListener("click", jogando, { once: true});
+    }
+
+    marcadoCirculo = false;
+
+    container.classList.add("x");
+}
+
+function checandoVencedor(jogadorAtual) {
+    
+}
 
 function marcandoEspaco(celula, adicionandoClasse) {
     celula.classList.add(adicionandoClasse);
 }
 
 function mudandoSimbolo() {
-    marcadoCirculo = !marcadoCirculo
-}
+    marcadoCirculo = !marcadoCirculo;
+
+    container.classList.remove("circulo");
+    container.classList.remove("x");
+
+    if(marcadoCirculo) {
+        container.classList.add("circulo");
+    } else {
+        container.classList.add("x");
+    }
+};
 
 function jogando(e) {
     //colocar o X ou Bolinha
@@ -23,6 +58,4 @@ function jogando(e) {
     mudandoSimbolo();
 }
 
-for (const celula of todasCelulas) {
-    celula.addEventListener("click", jogando, { once: true});
-}
+iniciarJogo();
